b43c73d4f0b259fa6e918fc2fc1ab21270e9571b
cit142f-4@M307.UC-BCF.EDU.PH <cit142f-4@M307.UC-BCF.EDU.PH>
2022-09-14 16:53:01 +0800

    -batch upload

---
+++ push.bat
@@ -0,0 +1,6 @@
+@echo off
+git remote set-url origin https://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@github.com/nicolebay-an/nicole22.git
+git add . 
+git commit -m -"batch upload"
+git push 
+pause 
